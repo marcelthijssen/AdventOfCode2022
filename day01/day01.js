49f65bb025688803a8ab41108ddcabdd8dcbf69f
marcelthijssen <info@studiothijssen.nl>
2022-12-01 19:47:27 +0100

    day01

--- day01/day01.js
+++ day01/day01.js
@@ -18,25 +18,22 @@ function part1() {
     const score = input[i].reduce( ( a, b ) => a + b, 0 );
     total.push( score );     // Add score to new array 'total'
   }
-
   const maxCalories = Math.max( ...total ); // find highest number in array
   console.log('maxCalories', maxCalories);
 }
 
 function part2() {
   const total = [];
-
   for ( let i = 0 ; i < input.length ; i++ ) {     // Add max to
-    const score = input[i].reduce( ( a, b ) => a + b, 0 );
+    const score = input[i].reduce( ( a, b ) => a + b, 0 );    // Using .reduce to add allnumbers from input[i]
     total.push( score );     // Add score to new array 'total'
   }
-
   let max = Math.max( ...total );
   let x = 1;
   let threeHighestScoreTotal=0;
-  while ( x < 4 ) {
+  while ( x <= 3 ) {
     max = Math.max( ...total );
-    threeHighestScoreTotal += max;     // Add max to
+    threeHighestScoreTotal += max;     // Add max to threeHighestScoreTotal
     const index = total.indexOf( max );     // find indexOf HighestNumber
     total.splice( index, 1 );    //remove index with the highestNumber
     x++;
