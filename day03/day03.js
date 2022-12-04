15fe4a4c30725e8d363d8c66baf4c59006462e55
marcelthijssen <info@studiothijssen.nl>
2022-12-04 10:10:41 +0100

    day3

--- day03/day03.js
+++ day03/day03.js
@@ -1,19 +1,17 @@
 const fs = require( "fs" );
 
-const items = fs
+const ruckSacks = fs
   .readFileSync( "day03.txt", { encoding: "utf-8" } ) // read day??.txt content
   .replace( /\r/g, "" ) // remove all \r characters to avoid issues on Windows
   .split( "\n" ) // Split on newline
   .filter( Boolean ); // Remove empty lines
 
-// console.log( items );
-
 function part1() {
 
-  let allChars =[];
+  let allChars = [];
   let scores = [];
-  items.forEach( ( line ) => {
-  let chars = [];
+  ruckSacks.forEach( ( line ) => {
+    let chars = [];
 
     let first = line.slice( 0, line.length / 2 ).split( "" );
     let second = line.slice( line.length / 2 ).split( "" );
@@ -24,17 +22,41 @@ function part1() {
         }
       }
     } );
-  allChars.push(...chars);
+    allChars.push( ...chars );
   } );
   allChars.forEach( ( char ) => {
     scores.push( char.charCodeAt( 0 ) > 96 ? char.charCodeAt( 0 ) - 96 : char.charCodeAt( 0 ) - 38 );
   } );
-    const totalScore = scores.reduce( ( a, b ) => a + b, 0 );
-    console.log( "Day03-part 1: totalScore", totalScore );
+  const totalScore = scores.reduce( ( a, b ) => a + b, 0 );
+  console.log( "Day03-part 1: totalScore", totalScore );
+}
+
+function part2() {
+
+  const allItems = [];
+  const priorities = [];
+  let i;
+  for ( i = 0 ; i < ruckSacks.length; i ) {
+  const items = [];
+    const ruckSack = ruckSacks[i];
+
+    for ( let j = 0 ; j <= ruckSack.length ; j++ ) {
+      if ( ruckSacks[i + 1].includes( ruckSack[j] ) && ruckSacks[i + 2].includes( ruckSack[j] ) ) {
+        if ( !items.includes( ruckSack[j] ) ) {
+          items.push( ...ruckSack[j] );
+        }
+      }
+    }
+    i+=3;
+    allItems.push( ...items );
+  }
+  allItems.forEach( ( char ) => {
+    priorities.push( char.charCodeAt( 0 ) > 96 ? char.charCodeAt( 0 ) - 96 : char.charCodeAt( 0 ) - 38 );
+  } );
+  const sumOfPriorities = priorities.reduce( ( a, b ) => a + b, 0 );
+  console.log( "Day03-part 2: totalScore", sumOfPriorities );
 }
 
 part1();
 
-/*
-parseInt( char.charCodeAt( 1 ) - 38 )
- */
+part2();
