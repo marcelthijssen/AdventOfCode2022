97c308cc04a7227ced9d91385b255ee7f04f3ba8
marcelthijssen <info@studiothijssen.nl>
2022-12-23 08:55:45 +0100

    day11

--- day11/day11.js
+++ day11/day11.js
@@ -1,7 +1,7 @@
 const fs = require( "fs" );
 let stringMath = require( "string-math" );
 
-let route = fs.readFileSync( "day11.txt", { encoding: "utf-8" } ) // read .txt content
+let monkeys = fs.readFileSync( "day11.txt", { encoding: "utf-8" } ) // read .txt content
   .replace( /\r/g, "" ) // remove all \r characters to avoid issues on Windows
   .split( "\n\n" )
   .filter( Boolean )
@@ -17,31 +17,27 @@ let route = fs.readFileSync( "day11.txt", { encoding: "utf-8" } ) // read .txt c
   } )
 ;
 
-console.log( route );
+// console.log( route );
 
 
 function part1() {
   // 20 rounds the monkey inspects and through
-  let monkey0 = route[0].items.length;
-  let monkey1 = route[1].items.length;
-  let monkey2 = route[2].items.length;
-  let monkey3 = route[3].items.length;
-  console.log( monkey0, monkey1, monkey2, monkey3 );
+
   for ( let i = 0 ; i < 20 ; i++ ) { // 20 times
 
-    for ( let j = 0 ; j < route.length ; j++ ) { // times monkeys
-      if ( route[j].items.length === undefined ) { // if monkey has no items
+    for ( let j = 0 ; j < monkeys.length ; j++ ) { // times monkeys
+      if ( monkeys[j].items.length === undefined ) { // if monkey has no items
 
       } else {
-        let items = route[j].items;
+        let items = monkeys[j].items;
         let newWorryLevel;
-        let operation = route[j].operation;
-        let istrue = route[j].istrue;
-        let isfalse = route[j].isfalse;
-        let test = route[j].test;
+        let operation = monkeys[j].operation;
+        let istrue = monkeys[j].istrue;
+        let isfalse = monkeys[j].isfalse;
+        let test = monkeys[j].test;
 
         while ( items.length > 0 ) { // number of items
-          route[j].times++;
+          monkeys[j].times++;
           let item = items.shift();
           if ( operation.slice( -3 ) === "old" ) {
             newWorryLevel = Math.floor( (item * item) / 3 );
@@ -52,19 +48,20 @@ function part1() {
           }
 
           if ( newWorryLevel % test === 0 ) {
-            route[istrue].items.push( newWorryLevel );
+            monkeys[istrue].items.push( newWorryLevel );
           } else {
-            route[isfalse].items.push( newWorryLevel );
+            monkeys[isfalse].items.push( newWorryLevel );
           }
         }
       }
     }
   }
-  route.sort( ( a, b ) => a.times - b.times );
-  const monkeyBusiness = route[route.length-1].times * route[route.length-2].times;
+  monkeys.sort( ( a, b ) => a.times - b.times );
+  const monkeyBusiness = monkeys[monkeys.length-1].times * monkeys[monkeys.length-2].times;
 
   console.log( "Day11 part 1. Monkey Business: ", monkeyBusiness );
 
 }
+
 
 part1();
